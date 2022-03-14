5fd03e8d7f5a6318411a3c219ec2c15ca7bc1a15
Miguel Teheran <miguelteheran@gmail.com>
2022-03-14 18:53:49 -0500

    agregando base de datos en memoria

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectoef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+
+});
 
 app.Run();
