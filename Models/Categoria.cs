832d2e4d5bb7779910ede72ca249311ea9ce5e7c
Miguel Teheran <miguelteheran@gmail.com>
2022-03-14 18:38:40 -0500

    dataannotations o atributos

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace proyectoef.Models;
 
 public class Categoria
 {
+    [Key]
     public Guid CategoriaId {get;set;}
+
+    [Required]
+    [MaxLength(150)]
     public string Nombre {get;set;}
     public string Descripcion {get;set;}
 
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace proyectoef.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId {get;set;}
+    
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId {get;set;}
 
+    [Required]
+    [MaxLength(200)]
     public string Titulo {get;set;}
 
     public string Descripcion {get;set;}
@@ -14,6 +22,9 @@ public class Tarea
     public DateTime FechaCreacion {get;set;}
     
     public virtual Categoria Categoria {get;set;}
+
+    [NotMapped]
+    public string Resumen {get;set;}
 }
 
 public enum Prioridad
