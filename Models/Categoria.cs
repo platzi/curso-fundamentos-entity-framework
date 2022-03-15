a03c3925baa20573ec020d7223a924ae36b751d1
Miguel Teheran <miguelteheran@gmail.com>
2022-03-15 15:49:58 -0500

    agregando categoria fluentapi

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -4,11 +4,11 @@ namespace proyectoef.Models;
 
 public class Categoria
 {
-    [Key]
+    //[Key]
     public Guid CategoriaId {get;set;}
 
-    [Required]
-    [MaxLength(150)]
+    //[Required]
+    //[MaxLength(150)]
     public string Nombre {get;set;}
     public string Descripcion {get;set;}
 
--- TareasContext.cs
+++ TareasContext.cs
@@ -9,4 +9,19 @@ public class TareasContext: DbContext
     public DbSet<Tarea> Tareas {get;set;}
 
     public TareasContext(DbContextOptions<TareasContext> options) :base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(categoria=> 
+        {
+            categoria.ToTable("Categoria");
+            categoria.HasKey(p=> p.CategoriaId);
+
+            categoria.Property(p=> p.Nombre).IsRequired().HasMaxLength(150);
+
+            categoria.Property(p=> p.Descripcion);
+        });
+
+    }
+
 }
