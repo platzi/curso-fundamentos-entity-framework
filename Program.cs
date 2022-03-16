4053dc662aa2d3da94c814ff3f24707957476ad1
Miguel Teheran <miguelteheran@gmail.com>
2022-03-15 19:00:32 -0500

    guarndado datos con EF

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyectoef;
+using proyectoef.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +21,19 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
 
 app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext)=>
 {
-    return Results.Ok(dbContext.Tareas.Include(p=> p.Categoria).Where(p=> p.PrioridadTarea == proyectoef.Models.Prioridad.Baja));
+    return Results.Ok(dbContext.Tareas.Include(p=> p.Categoria));
+});
+
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea)=>
+{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea);
+    //await dbContext.Tareas.AddAsync(tarea);
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();   
 });
 
 app.Run();
