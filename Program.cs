7df0091515f7b15e0bdcf2d1d45365cb36289456
Miguel Teheran <miguelteheran@gmail.com>
2022-03-16 15:22:58 -0500

    elimnando datos con EF

--- Program.cs
+++ Program.cs
@@ -56,4 +56,19 @@ app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [F
     return Results.NotFound();   
 });
 
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+     var tareaActual = dbContext.Tareas.Find(id);
+
+     if(tareaActual!=null)
+     {
+         dbContext.Remove(tareaActual);
+         await dbContext.SaveChangesAsync();
+
+         return Results.Ok();
+     }
+
+     return Results.NotFound();
+});
+
 app.Run();
