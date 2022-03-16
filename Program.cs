0283b895f36d9f81edd97eb3ca778688051b9ce9
Miguel Teheran <miguelteheran@gmail.com>
2022-03-16 15:00:55 -0500

    actualizando datos

--- Program.cs
+++ Program.cs
@@ -36,4 +36,24 @@ app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromB
     return Results.Ok();   
 });
 
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea,[FromRoute] Guid id)=>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if(tareaActual!=null)
+    {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+
+    }
+
+    return Results.NotFound();   
+});
+
 app.Run();
